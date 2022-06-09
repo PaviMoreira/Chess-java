a37a16351a128c0cac7b7fbc3244b07e12f4aed8
PaviMoreira <yahiko_pavi@live.com>
2022-06-08 21:11:44 -0300

    Movment function, attempt at coloring (to be finished later)

--- src/Application/Chess.java
+++ src/Application/Chess.java
@@ -1,14 +1,30 @@
 package Application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Chess {
 
 	public static void main(String[] args) {
 
+		Scanner read = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
-		
+
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(read);
+
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(read);
+
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
